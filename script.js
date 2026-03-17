2e59bdfc6ccd98cda3ab8927a3a00db6c21d40f8
MR KASHYAP <www.itsforbusiness@gmail.com>
2026-03-17 08:52:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function showPopup(){
+document.getElementById("popup").style.display="block";
+}
+
+function closePopup(){
+document.getElementById("popup").style.display="none";
+}
+
+function makeBold(el){
+el.style.fontWeight="bold";
+}
