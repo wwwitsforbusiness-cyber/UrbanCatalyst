69f2ad28d632d5d727008728fd1fe5113a5b0bf1
MR KASHYAP <www.itsforbusiness@gmail.com>
2026-03-17 09:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-// custom cursor logic
-const cursor = document.getElementById('cursor')
+const cursor = document.getElementById('cursor');
+
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px'
-    cursor.style.top = e.clientY + 'px'
-})
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
 
-// cursor reaction
 document.querySelectorAll('button, a, .s-card').forEach(item => {
-    item.addEventListener('mouseenter', () => cursor.style.transform = 'scale(5)')
-    item.addEventListener('mouseleave', () => cursor.style.transform = 'scale(1)')
-})
+    item.addEventListener('mouseenter', () => cursor.style.transform = 'scale(4)');
+    item.addEventListener('mouseleave', () => cursor.style.transform = 'scale(1)');
+});
 
-function showPopup() { document.querySelector('.modal-bg').style.display = 'flex' }
-function closePopup() { document.querySelector('.modal-bg').style.display = 'none' }
+function showPopup() { document.querySelector('.modal-bg').style.display = 'flex'; }
+function closePopup() { document.querySelector('.modal-bg').style.display = 'none'; }
+
+document.getElementById('leadForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('thank you for reaching out our team will connect with you soon');
+});
